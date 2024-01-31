6fc69e84471e05580c9d939eca9008aaf297dd30
Lorenzo Fratus <98fratus@gmail.com>
2024-01-31 11:44:07 +0100

    Reduce message size for set lights

--- src/message/c2v/SetLights.ts
+++ src/message/c2v/SetLights.ts
@@ -8,11 +8,11 @@ class SetLights extends AbstractVehicleMessage {
 	public readonly lightValue: number
 
 	public constructor(vehicleId: string, lightValue: number) {
-		super(vehicleId, Buffer.alloc(ANKI_VEHICLE_MSG_C2V_SET_LIGHTS_SIZE + 1));
+		super(vehicleId, Buffer.alloc(ANKI_VEHICLE_MSG_C2V_SET_LIGHTS_SIZE));
 
 		this.payload.writeUInt8(ANKI_VEHICLE_MSG_C2V_SET_LIGHTS_SIZE, 0);
 		this.payload.writeUInt8(ANKI_VEHICLE_MSG_C2V_SET_LIGHTS, 1);
-        this.payload.writeUInt8(lightValue, 2); // Bits 0-3 (mask), Bits 4-7 (Head/Tail/Brake/???)
+        this.payload.writeUInt8(lightValue, 2);
 		this.lightValue = lightValue;
 	}
 
